8fb41f79886ee0d99aa559eea30cec02ace3b26a
David Jurajda (nxa07326) <david.jurajda@nxp.com>
2022-03-07 16:49:16 +0100

    Apply MCUXpresso SDK 2.11.0 changes. Add cmakes.

---
+++ middleware_fatfs_usb.cmake
@@ -0,0 +1,147 @@
+#Description: Fatfs USB layer; user_visible: True
+include_guard(GLOBAL)
+message("middleware_fatfs_usb component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+)
+
+#OR Logic component
+if(CONFIG_USE_middleware_baremetal)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_bm.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1166_cm7)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1052)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1064)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MK64F12)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MK66F18)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC54628)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MK22F51212)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1021)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC55S16)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1062)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC54S018)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1176_cm7)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC54S018M)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC55S69_cm33_core0)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1024)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1011)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_LPC55S28)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_K32L2A41A)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT685S_cm33)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT595S_cm33)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+if(CONFIG_USE_middleware_freertos-kernel_MIMXRT1015)
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk/fsl_usb_disk_freertos.c
+)
+endif()
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/source/fsl_usb_disk
+)
+
+
+include(middleware_fatfs)
+include(middleware_usb_host_msd)
