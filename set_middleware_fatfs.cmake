34417a4bec19efb6ca2a3c6bdf23fdaa5de2b819
Mcu_sdk Ci <mcu_sdk.ci@nxp.com>
2024-04-19 09:40:40 +0200

    Apply 2.15.001 release update Add rt1180 support

--- set_middleware_fatfs.cmake
+++ set_middleware_fatfs.cmake
@@ -73,7 +73,7 @@ if (CONFIG_USE_middleware_fatfs)
 
 message("middleware_fatfs component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_USE_middleware_fatfs_template_ram AND CONFIG_USE_middleware_fatfs_ram) OR (CONFIG_USE_middleware_fatfs_template_sd AND CONFIG_USE_middleware_fatfs_sd) OR (CONFIG_USE_middleware_fatfs_template_sdspi AND CONFIG_USE_middleware_fatfs_sdspi) OR (CONFIG_USE_middleware_fatfs_template_mmc AND CONFIG_USE_middleware_fatfs_mmc) OR (CONFIG_USE_middleware_fatfs_template_usb AND CONFIG_USE_middleware_fatfs_usb))
+if((CONFIG_USE_middleware_fatfs_template_ram AND CONFIG_USE_middleware_fatfs_ram) OR (CONFIG_USE_middleware_fatfs_template_sd AND CONFIG_USE_middleware_fatfs_sd) OR (CONFIG_USE_middleware_fatfs_template_mmc AND CONFIG_USE_middleware_fatfs_mmc) OR (CONFIG_USE_middleware_fatfs_template_usb AND CONFIG_USE_middleware_fatfs_usb))
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
   ${CMAKE_CURRENT_LIST_DIR}/./source/diskio.c
@@ -161,30 +161,6 @@ target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
 else()
 
 message(SEND_ERROR "middleware_fatfs_sd dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_fatfs_sdspi)
-# Add set(CONFIG_USE_middleware_fatfs_sdspi true) in config.cmake to use this component
-
-message("middleware_fatfs_sdspi component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if(CONFIG_USE_middleware_fatfs AND CONFIG_USE_middleware_fatfs_template_sdspi AND CONFIG_USE_middleware_sdmmc_sdspi AND CONFIG_USE_driver_common AND CONFIG_USE_driver_dspi AND CONFIG_USE_driver_gpio)
-
-target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-  ${CMAKE_CURRENT_LIST_DIR}/./source/fsl_sdspi_disk/fsl_sdspi_disk.c
-)
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./source/fsl_sdspi_disk
-)
-
-else()
-
-message(SEND_ERROR "middleware_fatfs_sdspi dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
 
 endif()
 
